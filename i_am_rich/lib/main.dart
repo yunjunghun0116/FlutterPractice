310d98afff1339d288eb41b04eae1917607c6559
윤정훈 <yunjunghun0116@gmail.com>
2021-06-28 02:43:29 +0900

    asset image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 //option + enter => Wrap widget
+//image 1.NetworkImage 2.AssetImage( -> pubspec.yaml에서 수정)
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Center(child: Text('I Am Rich'),),
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/440px-Image_created_with_a_mobile_phone.png'),
+            image:AssetImage('images/diamond2.jpeg')
           ),
         ),
       ),
