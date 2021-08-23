1c0d1f6bafdb892469ca3456849eaea3ad18d0fc
윤정훈 <yunjunghun0116@gmail.com>
2021-08-23 20:05:58 +0900

    read me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FlutterPractice
+
+Flutter를 연습하면서
+새로운 라이브러리들을 접해보고
+GetX나 provider를 통한 상태관리를 연습하기위한 프로젝트모음
---
+++ when_feed/android/app/src/main/kotlin/com/example/when_feed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.when_feed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
