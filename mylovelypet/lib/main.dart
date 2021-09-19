1dacd717121fc4b27df62227e20ea0067c957bab
윤정훈 <yunjunghun0116@gmail.com>
2021-09-20 02:39:09 +0900

    my pet

---
+++ mylovelypet/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'screens/home_screen.dart';
+import 'screens/detail_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: ()=>HomeScreen()),
+        GetPage(name: '/detail/:petMomentId', page: ()=>DetailScreen()),
+      ],
+    );
+  }
+}
+
---
+++ mylovelypet/lib/model/pet_moment.dart
@@ -0,0 +1,20 @@
+//  DB로부터 데이터를 PetMoment형식으로 다 가져와서 로드해줄것이기때문에 모델이 필요하고
+class PetMoment {
+  //여기서 id는 데이터 저장했을때 이미지를 구분하기위한 작업 & changeComment를 해줄때 사용될 id
+  String id;
+  String imageUrl;
+  String date;
+  String comment;
+
+  PetMoment(
+      {required this.id,required this.imageUrl, required this.date, required this.comment});
+
+  void changeComment(String s){
+    this.comment = s;
+    //TODO 변경된 comment를 새로 저장해주는 방식
+  }
+
+  String get momentImageUrl => imageUrl;
+  String get momentDate => date;
+  String get momentComment => comment;
+}
