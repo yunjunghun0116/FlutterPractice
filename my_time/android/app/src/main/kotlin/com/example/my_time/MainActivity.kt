bf84578accd2ac99cd097fad722cfb1791d3091e
윤정훈 <yunjunghun0116@gmail.com>
2021-09-20 01:55:29 +0900

    mypets

--- my_time/android/app/src/main/kotlin/com/example/my_time/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_time
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ mypetmoments/android/app/src/main/kotlin/com/example/mypetmoments/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mypetmoments
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
