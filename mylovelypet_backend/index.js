bd5d9a5bc9dc31397bc82b596dbee0b01956bc28
윤정훈 <yunjunghun0116@gmail.com>
2021-09-20 20:37:45 +0900

    connect mongodb and backend

--- mylovelypet_backend/index.js
+++ mylovelypet_backend/index.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-
 //mylovelypet@naver.com a1041714 mongodb
 //connect username : mylovelypet password : a1041714
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
 const uri =
   "mongodb+srv://mylovelypet:a1041714@mylovelypet.yrrw5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const port = 3000;
 
-mongoose.connect(uri, {});
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 //서버를 어디서 열지 -> 서버를 띄울 포트번호, 띄운 후 실행할 코드
 app.listen(port, function () {
