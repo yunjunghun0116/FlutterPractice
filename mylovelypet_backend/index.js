af713f61af18cf28ee78f98ffa94dc12a2799485
윤정훈 <yunjunghun0116@gmail.com>
2021-09-24 16:25:59 +0900

    mylovelypet connect

--- mylovelypet_backend/index.js
+++ mylovelypet_backend/index.js
@@ -1,14 +1,12 @@
 //mylovelypet@naver.com a1041714 mongodb
 //connect username : mylovelypet password : a1041714
 const express = require("express");
-const router = express.Router();
-const multer = require("multer");
 const mongoose = require("mongoose");
 const app = express();
 
 const uri =
   "mongodb+srv://mylovelypet:a1041714@mylovelypet.yrrw5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const port = 3000;
+const port = 5000;
 
 mongoose
   .connect(uri, {
@@ -21,31 +19,8 @@ mongoose
 //서버를 어디서 열지 -> 서버를 띄울 포트번호, 띄운 후 실행할 코드
 app.listen(port, () => console.log(`listening on ${port}`));
 
-app.get("/pet", function (req, res) {
-  return res.send("thanks");
-});
+app.use("/api/users", require("./routes/users"));
 
-//이미지 저장관련 코드
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}_${file.originalname}`);
-  },
-});
+app.use("/api/product", require("./routes/product"));
 
-var upload = multer({ storage: storage }).single("file");
-router.post("/image", (req, res) => {
-  //가져온이미지저장
-  upload(req, res, (err) => {
-    if (err) {
-      return res.json({ success: false, err });
-    }
-    return res.json({
-      success: true,
-      filePath: res.req.file.path,
-      fileName: res.req.file.filename,
-    });
-  });
-});
+app.use("/uploads", express.static("uploads"));
