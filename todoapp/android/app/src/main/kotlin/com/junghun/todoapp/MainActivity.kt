475f8dcd77f03dcd1a9aa200561531dc36b9764f
junghun <yunjunghun0116@gmail.com>
2022-04-15 18:06:08 +0900

    todoapp

---
+++ todoapp/android/app/src/main/kotlin/com/junghun/todoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.junghun.todoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ todoapp/lib/screens/main/main_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/screens/home/home_screen.dart';
+import 'package:todoapp/screens/manage/manage_screen.dart';
+import 'package:todoapp/screens/user/user_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  Widget _getScreen() {
+    switch (_currentIndex) {
+      case 1:
+        return ManageScreen();
+      case 2:
+        return UserScreen();
+      case 0:
+      default:
+        return HomeScreen();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _getScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.manage_search_outlined),
+            label: 'Todo관리',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: '마이페이지',
+          ),
+        ],
+      ),
+    );
+  }
+}
