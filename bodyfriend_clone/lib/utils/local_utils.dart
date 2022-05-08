f90327cea62c8baeb3b7f867ec79e4b8332cfe14
junghun <yunjunghun0116@gmail.com>
2022-05-08 16:11:49 +0900

    date logic

--- bodyfriend_clone/lib/utils/local_utils.dart
+++ bodyfriend_clone/lib/utils/local_utils.dart
@@ -4,5 +4,12 @@ String timeDifference(
   int onAwakeHours,
   int onAwakeMinutes,
 ) {
-  return '09:00';
+  int _differenceTime =
+      (onAwakeHours - onSleepHours) * 60 + (onAwakeMinutes - onSleepMinutes);
+  if (_differenceTime < 0) {
+    _differenceTime += 1440;
+  }
+  int _hours = (_differenceTime / 60).floor();
+  int _minutes = _differenceTime % 60;
+  return '${_hours >= 10 ? _hours : '0$_hours'}:${_minutes >= 10 ? _minutes : '0$_minutes'}';
 }
